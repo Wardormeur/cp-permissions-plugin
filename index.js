f34d91fec0c13369fc37d165f6c0279bebddc4bc
Wardormeur <wardormeur@gmail.com>
2016-06-08 11:26:20 +0100

    Handle cross-lib calls with proper tokenization

--- index.js
+++ index.js
@@ -2,53 +2,91 @@
 
 var _ = require('lodash');
 var async = require('async');
-var crypto = require('crypto');
+var tokenizer = require('./lib/tokenizer');
 
 module.exports = function (options) {
   var seneca = this;
   var plugin = 'cd-permissions';
+
+  //  Required params
   var config = require(options.config)();
+  var clients = options.clients;
 
   seneca.add({role: 'cd-permissions', cmd: 'check_permissions'},
     require('./lib/check_permissions').bind( _.extend(_.clone(this), {permConfig: config}) ));
 
+  seneca.root.context = {};
+
+  var wrapTokenization = function (done) {
+    _.each(clients, function(lib){
+      seneca.wrap({role: lib}, function(msg, respond){
+        console.log('original', msg);
+        if(_.isUndefined(msg.perm)){
+          msg = getContext(msg);
+          console.log('extended', msg);
+        }
+        this.prior(msg, respond);
+      });
+    });
+    done();
+  };
+
+
   var wrapLib = function (lib) {
     seneca.wrap({role: lib}, function(msg, respond){
-      if(!msg.meta$.token && !msg.meta$.hash || !verifyToken(msg.meta$.token, msg.meta$.hash)){
+      if(!msg.perm
+        || (!msg.perm.token && !msg.perm.hash)
+        || !tokenizer.isValid(msg.perm.token, msg.perm.hash)){
+        console.log('need validation');
         seneca.act({role: 'cd-permissions', cmd: 'check_permissions', msg: msg},
           (function (err, response){
             if(response && !_.isObject(response)){
-              msg.meta$.token = Date.now().toString();
-              msg.meta$.hash = createToken(msg.meta$.token);
-              this.prior(msg, respond);
+              // Doesn't this mean that every call used on permissions as a check is allowed to do anything? MegaCare here
+              msg.perm = {};
+              msg.perm.token = Date.now().toString();
+              msg.perm.hash = tokenizer.createToken(msg.perm.token);
+              setContext(msg);
+              this.prior(msg, function(a, b) {
+                console.log('respond', respond)
+                if(msg.transport$){
+                  console.log('token revoked');
+                  revokeToken(msg.id);
+                }
+                return respond(a, b);
+              });
             } else {
               respond(null, {http$: response});
             }
           }).bind(this)
         );
       } else {
+        setContext(msg);
         this.prior(msg, respond);
       }
     });
   };
 
-  _.each(_.keys(config), wrapLib);
-
-
-  var verifyToken =  function (token, hash) {
-    var valid = false;
-    if (createToken(token) === hash){
-        valid = true;
-    }
-    return valid;
+  var setContext = function(msg) {
+    seneca.root.context[msg.id] = {token: msg.perm.token, hash: msg.perm.hash};
   };
 
-  var createToken = function (token) {
-    var hash = crypto.createHmac('sha512', process.env.PERM_MASTERKEY);
-    hash.update(token);
-    return hash.digest('hex');
+  var getContext = function(msg) {
+    return _.extend(msg, {perm: seneca.root.context[msg.id]});
   };
 
+  var revokeToken = function(id) {
+    delete seneca.root.context[id];
+  };
+
+  async.waterfall(
+    [
+    function(done){
+      _.each(_.keys(config), wrapLib);
+      done();
+    },
+    wrapTokenization,
+  ]
+  );
 
   return {
     name: plugin
